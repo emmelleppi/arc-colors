077695d31ff4dd85f91b660c80e3fb2ead79c744
emmelleppi <mlp.perego@gmail.com>
2021-01-22 19:38:35 +0100

    Adds glass model

--- src/store.js
+++ src/store.js
@@ -5,11 +5,11 @@ export const MAX_INDEX = 900
 export const INIT_INDEX = MAX_INDEX / 2
 export const NUM = 8
 export const useWheel = create((set) => ({
-    wheelIndex: INIT_INDEX,
-    wheelOpen: false,
-    palette: niceColorPalette[INIT_INDEX],
-    setPalette: (palette) => set({ palette }),
-    increaseWheelIndex: () => set((state) => ({ wheelIndex: state.wheelIndex + 1 })),
-    decreaseWheelIndex: () => set((state) => ({ wheelIndex: state.wheelIndex - 1 })),
-    toggleWheel: () => set((state) => ({ wheelOpen: !state.wheelOpen }))
-  }))
+  wheelIndex: INIT_INDEX,
+  wheelOpen: false,
+  palette: niceColorPalette[INIT_INDEX],
+  setPalette: (palette) => set({ palette }),
+  increaseWheelIndex: () => set((state) => ({ wheelIndex: state.wheelIndex + 1 })),
+  decreaseWheelIndex: () => set((state) => ({ wheelIndex: state.wheelIndex - 1 })),
+  toggleWheel: () => set((state) => ({ wheelOpen: !state.wheelOpen }))
+}))
